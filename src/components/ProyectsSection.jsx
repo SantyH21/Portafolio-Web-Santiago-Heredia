8f5f60fb7df1ee5ff9413b7d2062c3ebac47942b
SantyH21 <santyheredia2017@gmail.com>
2025-10-27 19:56:22 -0300

    Agregado Proyectos
    Agregado Proyectos, imagenes, etc.

--- src/components/ProyectsSection.jsx
+++ src/components/ProyectsSection.jsx
@@ -5,10 +5,60 @@ const proyectos = [
     {
         id: 1,
         title: "Martina Cespedes Backend",
-        description: "Proyecto de backend para gestionar datos de usuarios y productos.",
-        imageUrl: "proyectsPics/portafolio.png",
+        description: "Proyecto de backend para gestionar datos de usuarios, stock de productos y materia prima de una fabrica de Gin.",
+        imageUrl: "proyectsPics/BackendMartinaC.png",
         tags: ["Node.js", "Express", "Prisma", "PostgreSQL"],
-        gitHubUrl: "#",
+        gitHubUrl: "https://github.com/SantyH21/Martina-Cespedes/tree/dev",
+        demoUrl: "#",
+    },
+
+    {
+        id: 2,
+        title: "Portafolio Personal Web",
+        description: "Proyecto de Portafolio Web Personal para mostrar mis habilidades y proyectos como desarrollador. desarrollado con React y Tailwind CSS.",
+        imageUrl: "proyectsPics/portafolio.png",
+        tags: ["Node.js", "React", "Tailwind CSS"],
+        gitHubUrl: "https://github.com/SantyH21/Portafolio-Web-Santiago-Heredia",
+        demoUrl: "#",
+    },
+
+    {
+        id: 3,
+        title: "Tesis Final de Carrera que buscaba crear un sistema de alerta temprana de abandono escolar, a traves de datos recolectados brindados por el ministerio de educacion.",
+        description: "Tesis final que consistia",
+        imageUrl: "proyectsPics/.png",
+        tags: ["Python", "Pandas", "Scikit-learn", "Matplotlib", "PyCaret", "GoogleColab"],
+        gitHubUrl: "https://drive.google.com/drive/u/2/folders/1nUNoAl4pgiqL0fo4vBfzjY8BcZ9vQyAk",
+        demoUrl: "#",
+    },
+
+    {
+        id: 4,
+        title: "Gestor de recetas de cocina",
+        description: "Gestor de recetas de cocina desarrollado con React, CSS y JavaScript, que permite a los usuarios buscar, agregar y organizar recetas de manera sencilla.",
+        imageUrl: "proyectsPics/recetasCocina.png",
+        tags: ["React", "CSS" , "JavaScript"],
+        gitHubUrl: "https://github.com/SantyH21/Recetas-De-Cocina",
+        demoUrl: "#",
+    },
+
+    {
+        id: 5,
+        title: "Analizador Lexico / proyecto de compiladores / facultad",
+        description: "Proyecto de analizador léxico desarrollado en Python, que procesa código fuente para identificar tokens y estructuras sintácticas.",
+        imageUrl: "proyectsPics/analizadorLexico.png",
+        tags: ["Python"],
+        gitHubUrl: "https://github.com/SantyH21/Analizador-Lexico",
+        demoUrl: "#",
+    },
+
+    {
+        id: 6,
+        title: "Analizador Sintactico / proyecto de compiladores / facultad",
+        description: "Proyecto de analizador sintactico desarrollado en Python, que procesa código fuente para identificar tokens y estructuras sintácticas.",
+        imageUrl: "proyectsPics/analizadorSintactico.png",
+        tags: ["Python"],
+        gitHubUrl: "https://github.com/SantyH21/Analizador-Sintactico",
         demoUrl: "#",
     },
 
@@ -52,7 +102,7 @@ export const ProyectsSection = () => {
                             <div className='flex items-center justify-between'>
                                 <div className='flex space-x-3'>
                                     <a href={proyecto.demoUrl} target="_blank" className='text-foreground/80 hover:text-primary transition-colors duration-300'><ExternalLink size={20} /></a>
-                                    <a href={proyecto.gitHubUrl} target="_blank" className='text-foreground/80 hover:text-primary transition-colors duration-300'><GithubIcon size={20}/></a>
+                                    <a href={proyecto.gitHubUrl} target="_blank" className='text-foreground/80 hover:text-primary transition-colors duration-300'><GithubIcon size={20} /></a>
                                 </div>
                             </div>
                         </div>
@@ -60,7 +110,7 @@ export const ProyectsSection = () => {
                 </div>
 
                 <div className='text-center mt-12'>
-                    <a  href= "https://github.com/SantyH21" target="_blank" className='cosmic-button opacity-70 w-fit flex items-center mx-auto gap-2'>Mi cuenta de GitHub <LucideGithub size={16}/></a>
+                    <a href="https://github.com/SantyH21" target="_blank" className='cosmic-button opacity-70 w-fit flex items-center mx-auto gap-2'>Mi cuenta de GitHub <LucideGithub size={16} /></a>
                 </div>
             </div>
         </section>
